8ec3c65a962ced209bc32ddc5fc069a957be9024
George Wang <George.Wang@ehi.com>
2017-12-03 23:20:12 -0600

    Solving Day 4.

---
+++ src/Day4.kt
@@ -0,0 +1,77 @@
+import java.util.*
+import kotlin.collections.HashMap
+
+fun main(args: Array<String>) {
+//    println("Hello world!")
+    part4_1()
+    part4_2()
+}
+
+fun part4_1() {
+    val scan = Scanner(ClassLoader.getSystemResourceAsStream("resources/Day4_1.txt"))
+    var valid = 0
+    while (scan.hasNextLine()) {
+        val line = scan.nextLine()
+        val split = line.split(" ")
+        if (isValid(split)) {
+            valid++
+        }
+
+    }
+    println("valid=" + valid)
+}
+
+fun part4_2() {
+    val scan = Scanner(ClassLoader.getSystemResourceAsStream("resources/Day4_1.txt"))
+    var valid = 0
+    while (scan.hasNextLine()) {
+        val line = scan.nextLine()
+        val split = line.split(" ")
+        if (isValid2(split)) {
+            valid++
+        }
+
+    }
+    println("valid=" + valid)
+}
+
+fun isValid2(split: List<String>): Boolean {
+    var set = HashSet<HashMap<Char, Int>>()
+    for (word in split) {
+        val wordSoup = toWordSoup(word)
+        if (set.contains(wordSoup)) {
+            return false
+        }
+        set.add(wordSoup)
+    }
+    return true
+}
+
+fun toWordSoup(input : String): HashMap<Char, Int> {
+    var result = HashMap<Char, Int>()
+    for (c in input) {
+        // Lesson: optimization using the optional -- default to a sensible value and always override
+        var occur = result.get(c) ?: 0
+        occur++
+        result.put(c, occur)
+//        if (result.containsKey(c)) {
+//            var occur: Int = result.get(c) ?: 0 // Lesson: dealing with Optional like ? results
+//            occur += 1
+//            result.put(c, occur)
+//        } else {
+//            result.put(c, 1)
+//        }
+    }
+    return result
+}
+
+fun isValid(split: List<String>): Boolean {
+    var set = HashSet<String>()
+    for (word in split) {
+        if (set.contains(word)) {
+            return false
+        }
+        set.add(word)
+    }
+    return true
+}
